1f1a4d3c1cfe69671417d2ec506b7b6874ff7e9e
rayyanmohsin1 <rmohsin@my.centennialcollege.ca>
2024-11-28 20:29:56 +0500

    Added database access message and finalized basic code

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace AzureDevOpsQuizApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Azure DevOps Quiz App");
-            Console.WriteLine("Database access has already been done on Nov 28, 2024, by Rayyan Mohsin");
+            Console.WriteLine("Welcome to Azure DevOps Quiz 4 App");
+            Console.WriteLine("Database access has already been done on Nov 28, 2024, by Rayyan Mohsin ");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
