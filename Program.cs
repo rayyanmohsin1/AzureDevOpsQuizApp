47250153c0d73967accb25be5710b09c83262839
rayyanmohsin1 <rmohsin@my.centennialcollege.ca>
2024-11-28 20:03:45 +0500

    Initial commit with basic project structure

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AzureDevOpsQuizApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Azure DevOps Quiz App");
+            Console.WriteLine("Database access has already been done on Nov 28, 2024, by Muhammad Shahzad");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
